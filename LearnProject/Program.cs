05e267a57a075d41ef5fd6f1d7b988cd5c3e85c5
Gary <gongxicheng1111@hotmail.com>
2018-04-06 11:31:48 +0800

    change c# version

--- LearnProject/Program.cs
+++ LearnProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace LearnProject
     {
         static void Main(string[] args)
         {
+            var path = Console.ReadLine();
+            if(Directory.Exists(path))
+            {
+
+            }
         }
     }
 }
